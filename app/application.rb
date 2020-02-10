f8333fbbc890232271fcdb5302b4d53625d1fe66
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-10 13:00:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
     resp = Rack::Response.new
  
     this_moment = Time.new 
-    if this_moment.hour < 12
-      resp.write "Good Morning!"
-    else
+    if this_moment.hour > 12
       resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
     end
  
     resp.finish
